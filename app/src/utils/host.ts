447cf4f67b8170bf4237730999db8ebf23e84ad2
Uzini <xxenzoxx916@gmail.com>
2023-12-20 18:41:25 +0100

    fix: EXPO_PUBLIC_USE_ENV_HOST in host.ts

--- app/src/utils/host.ts
+++ app/src/utils/host.ts
@@ -11,7 +11,7 @@ let tempPort = "3000";
 if (process.env.EXPO_PUBLIC_HOST && process.env.EXPO_PUBLIC_USE_ENV_HOST === "true") {
   tempHost = process.env.EXPO_PUBLIC_HOST;
 }
-if (process.env.EXPO_PUBLIC_PORT && process.env.EXPO_PUBLIC_USE_ENV_PORT === "true") {
+if (process.env.EXPO_PUBLIC_PORT && process.env.EXPO_PUBLIC_USE_ENV_HOST === "true") {
   tempPort = process.env.EXPO_PUBLIC_PORT;
 }
 
